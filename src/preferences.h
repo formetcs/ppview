0d4e0f2273283ddade8db845b3b6799cd68144df
Stefan Dillmann <stefandillmann@gmx.de>
2025-02-15 23:11:01 +0100

    API documentation

--- src/preferences.h
+++ src/preferences.h
@@ -27,111 +27,465 @@
 #include <QtCore>
 
 
+/*!
+ * \brief Class to provide global access to the application settings.
+ *
+ * This class is realized using the Singleton Design Pattern and cannot be instantiated directly.
+ * Use the static getInstance() method to get global access to the settings.
+ *
+ * All settings are stored persistently using the QSettings class.
+ */
 class Preferences
 {
 public:
+    /*!
+     * \brief Get the Preferences instance.
+     *
+     * \return the Preferences instance
+     */
     static Preferences* getInstance();
 
+    /*!
+     * \brief Read the settings from disk.
+     */
     void readSettings();
+
+    /*!
+     * \brief Write the settings to disk.
+     */
     void writeSettings();
+
+    /*!
+     * \brief Set the default values.
+     */
     void setDefaultValues();
 
+    /*!
+     * \brief Get the position of the main window.
+     *
+     * \return the position of the main window
+     */
     QPoint getPos();
+
+    /*!
+     * \brief Set the position of the main window.
+     *
+     * \param p the position of the main window
+     */
     void setPos(QPoint p);
+
+    /*!
+     * \brief Get the size of the main window.
+     *
+     * \return the size of the main window
+     */
     QSize getSize();
+
+    /*!
+     * \brief Set the size of the main window.
+     *
+     * \param s the size of the main window
+     */
     void setSize(QSize s);
+
+    /*!
+     * \brief Get the window state of the main window.
+     *
+     * \return the window state of the main window
+     */
     QByteArray getWindowState();
+
+    /*!
+     * \brief Set the window state of the main window.
+     *
+     * \param ws the window state of the main window
+     */
     void setWindowState(QByteArray ws);
 
+    /*!
+     * \brief Get the active language.
+     *
+     * \return the active language
+     */
     QString getLanguage();
+
+    /*!
+     * \brief Set the active language.
+     *
+     * \param lang the active language
+     */
     void setLanguage(QString lang);
+
+    /*!
+     * \brief Get the zoom step.
+     *
+     * This is the zoom step that is used when one of the zoom actions has been clicked.
+     *
+     * \return the zoom step
+     */
     int getZoomStep();
+
+    /*!
+     * \brief Set the zoom step.
+     *
+     * This is the zoom step that is used when one of the zoom actions has been clicked.
+     *
+     * \param step the zoom step
+     */
     void setZoomStep(int step);
+
+    /*!
+     * \brief Get the minimum zoom value.
+     *
+     * \return the minimum zoom value
+     */
     int getMinZoom();
+
+    /*!
+     * \brief Set the minimum zoom value.
+     *
+     * \param min the minimum zoom value
+     */
     void setMinZoom(int min);
+
+    /*!
+     * \brief Get the rotate step.
+     *
+     * This is the rotate step that is used when one of the rotate actions has been clicked.
+     *
+     * \return the rotate step
+     */
     int getRotateStep();
+
+    /*!
+     * \brief Set the rotate step.
+     *
+     * This is the rotate step that is used when one of the rotate actions has been clicked.
+     *
+     * \param step the rotate step
+     */
     void setRotateStep(int step);
+
+    /*!
+     * \brief Get the maximum recursion depth.
+     *
+     * This is the recursion depth used for the search for referencing objects.
+     *
+     * \return the maximum recursion depth
+     */
     int getMaxRecursionDepth();
+
+    /*!
+     * \brief Set the maximum recursion depth.
+     *
+     * This is the recursion depth used for the search for referencing objects.
+     *
+     * \param depth the maximum recursion depth
+     */
     void setMaxRecursionDepth(int depth);
 
+    /*!
+     * \brief Get the list of all registered SMT solvers.
+     *
+     * \return the list of SMT solvers (as their identifier string)
+     */
     QStringList getSmtList();
+
+    /*!
+     * \brief Clear the list of SMT solvers.
+     */
     void clearSmtList();
+
+    /*!
+     * \brief Get the command for a given SMT id.
+     *
+     * \param smtId the smt id
+     * \return the command
+     */
     QString getSmtCommand(QString smtId);
+
+    /*!
+     * \brief Set the command for a given SMT id.
+     *
+     * \param smtId the smt id
+     * \param command the command
+     */
     void setSmtCommand(QString smtId, QString command);
+
+    /*!
+     * \brief Get the arguments for a given SMT id.
+     *
+     * \param smtId the smt id
+     * \return the arguments
+     */
     QString getSmtArguments(QString smtId);
+
+    /*!
+     * \brief Set the arguments for a given SMT id.
+     *
+     * \param smtId the smt id
+     * \param arguments the arguments
+     */
     void setSmtArguments(QString smtId, QString arguments);
 
+    /*!
+     * \brief Get the filter state for a given PlanPro object type.
+     *
+     * \param filter the PlanPro object type
+     * \return true if the object should be shown, false if it should be hidden
+     */
     bool getFilterSetting(QString filter);
+
+    /*!
+     * \brief Set the filter state for a given PlanPro object type.
+     *
+     * \param filter the PlanPro object type
+     * \param enabled true if the object should be shown, false if it should be hidden
+     */
     void setFilterSetting(QString filter, bool enabled);
 
+    /*!
+     * \brief Get the graphics view pen for a given PlanPro object type.
+     *
+     * \param type the PlanPro object type
+     * \return the pen
+     */
     QPen getGraphicsViewPen(QString type);
+
+    /*!
+     * \brief Set the graphics view pen for a given PlanPro object type.
+     *
+     * \param type the PlanPro object type
+     * \param pen the pen
+     */
     void setGraphicsViewPen(QString type, QPen pen);
+
+    /*!
+     * \brief Get the graphics view brush for a given PlanPro object type.
+     *
+     * \param type the PlanPro object type
+     * \return the brush
+     */
     QBrush getGraphicsViewBrush(QString type);
+
+    /*!
+     * \brief Set the graphics view brush for a given PlanPro object type.
+     *
+     * \param type the PlanPro object type
+     * \param brush the brush
+     */
     void setGraphicsViewBrush(QString type, QBrush brush);
 
+    /*!
+     * \brief Get the foreground brush for object list items in the start state (combined view only).
+     *
+     * \return the brush
+     */
     QBrush getObjectListFgStartBrush();
+
+    /*!
+     * \brief Set the foreground brush for object list items in the start state (combined view only).
+     *
+     * \param brush the brush
+     */
     void setObjectListFgStartBrush(QBrush brush);
+
+    /*!
+     * \brief Get the background brush for object list items in the start state (combined view only).
+     *
+     * \return the brush
+     */
     QBrush getObjectListBgStartBrush();
+
+    /*!
+     * \brief Set the background brush for object list items in the start state (combined view only).
+     *
+     * \param brush the brush
+     */
     void setObjectListBgStartBrush(QBrush brush);
+
+    /*!
+     * \brief Get the foreground brush for object list items in the end state (combined view only).
+     *
+     * \return the brush
+     */
     QBrush getObjectListFgEndBrush();
+
+    /*!
+     * \brief Set the foreground brush for object list items in the end state (combined view only).
+     *
+     * \param brush the brush
+     */
     void setObjectListFgEndBrush(QBrush brush);
+
+    /*!
+     * \brief Get the background brush for object list items in the end state (combined view only).
+     *
+     * \return the brush
+     */
     QBrush getObjectListBgEndBrush();
+
+    /*!
+     * \brief Set the background brush for object list items in the end state (combined view only).
+     *
+     * \param brush the brush
+     */
     void setObjectListBgEndBrush(QBrush brush);
+
+    /*!
+     * \brief Get the foreground brush for object list items in both states (combined view only).
+     *
+     * \return the brush
+     */
     QBrush getObjectListFgBothBrush();
+
+    /*!
+     * \brief Set the foreground brush for object list items in both states (combined view only).
+     *
+     * \param brush the brush
+     */
     void setObjectListFgBothBrush(QBrush brush);
+
+    /*!
+     * \brief Get the background brush for object list items in both states (combined view only).
+     *
+     * \return the brush
+     */
     QBrush getObjectListBgBothBrush();
+
+    /*!
+     * \brief Set the background brush for object list items in both states (combined view only).
+     *
+     * \param brush the brush
+     */
     void setObjectListBgBothBrush(QBrush brush);
 
+    /*!
+     * \brief Get the pen for graphics view items in the start state (combined view only).
+     *
+     * \return the pen
+     */
     QPen getGraphicsViewStartPen();
+
+    /*!
+     * \brief Set the pen for graphics view items in the start state (combined view only).
+     *
+     * \param pen the pen
+     */
     void setGraphicsViewStartPen(QPen pen);
+
+    /*!
+     * \brief Get the brush for graphics view items in the start state (combined view only).
+     *
+     * \return the brush
+     */
     QBrush getGraphicsViewStartBrush();
+
+    /*!
+     * \brief Set the brush for graphics view items in the start state (combined view only).
+     *
+     * \param brush the brush
+     */
     void setGraphicsViewStartBrush(QBrush brush);
+
+    /*!
+     * \brief Get the pen for graphics view items in the end state (combined view only).
+     *
+     * \return the pen
+     */
     QPen getGraphicsViewEndPen();
+
+    /*!
+     * \brief Set the pen for graphics view items in the end state (combined view only).
+     *
+     * \param pen the pen
+     */
     void setGraphicsViewEndPen(QPen pen);
+
+    /*!
+     * \brief Get the brush for graphics view items in the end state (combined view only).
+     *
+     * \return the brush
+     */
     QBrush getGraphicsViewEndBrush();
+
+    /*!
+     * \brief Set the brush for graphics view items in the end state (combined view only).
+     *
+     * \param brush the brush
+     */
     void setGraphicsViewEndBrush(QBrush brush);
+
+    /*!
+     * \brief Get the pen for graphics view items in both states (combined view only).
+     *
+     * \return the pen
+     */
     QPen getGraphicsViewBothPen();
+
+    /*!
+     * \brief Set the pen for graphics view items in both states (combined view only).
+     *
+     * \param pen the pen
+     */
     void setGraphicsViewBothPen(QPen pen);
+
+    /*!
+     * \brief Get the brush for graphics view items in both states (combined view only).
+     *
+     * \return the brush
+     */
     QBrush getGraphicsViewBothBrush();
+
+    /*!
+     * \brief Set the brush for graphics view items in both states (combined view only).
+     *
+     * \param brush the brush
+     */
     void setGraphicsViewBothBrush(QBrush brush);
 
 protected:
+    /*!
+     * \brief Constructor.
+     *
+     * Direct object creation is disabled. Use the static getInstance() method to get global access to the settings.
+     */
     Preferences();
 
 private:
-    static Preferences* preferences;
+    static Preferences* preferences; /*!< \brief The Preferences instance (as singleton). */
 
-    QPoint pos;
-    QSize size;
-    QByteArray windowState;
+    QPoint pos; /*!< \brief The position of the main window. */
+    QSize size; /*!< \brief The size of the main window. */
+    QByteArray windowState; /*!< \brief The window state of the main window. */
 
-    QString language;
-    int zoomStep;
-    int minZoom;
-    int rotateStep;
-    int maxRecursionDepth;
+    QString language; /*!< \brief The active language. */
+    int zoomStep; /*!< \brief The zoom step. */
+    int minZoom; /*!< \brief The minimum zoom size. */
+    int rotateStep; /*!< \brief The rotate step. */
+    int maxRecursionDepth; /*!< \brief The maximum recursion depth. */
 
-    QHash<QString, QString> smtCommands;
-    QHash<QString, QString> smtArguments;
+    QHash<QString, QString> smtCommands; /*!< \brief Hash map containing the commands for each SMT solver. */
+    QHash<QString, QString> smtArguments; /*!< \brief Hash map containing the command line arguments for each SMT solver. */
 
-    QHash<QString, bool> filterSettings;
+    QHash<QString, bool> filterSettings; /*!< \brief Hash map containing the filter settings for each object type. */
 
-    QHash<QString, QPen> graphicsViewPens;
-    QHash<QString, QBrush> graphicsViewBrushes;
+    QHash<QString, QPen> graphicsViewPens; /*!< \brief Hash map containing the graphics view pens for each object type. */
+    QHash<QString, QBrush> graphicsViewBrushes; /*!< \brief Hash map containing the graphics view brushes for each object type. */
 
-    QBrush objectListFgStartBrush;
-    QBrush objectListBgStartBrush;
-    QBrush objectListFgEndBrush;
-    QBrush objectListBgEndBrush;
-    QBrush objectListFgBothBrush;
-    QBrush objectListBgBothBrush;
+    QBrush objectListFgStartBrush; /*!< \brief Foreground brush for object list items in the start state (combined view only). */
+    QBrush objectListBgStartBrush; /*!< \brief Background brush for object list items in the start state (combined view only). */
+    QBrush objectListFgEndBrush; /*!< \brief Foreground brush for object list items in the end state (combined view only). */
+    QBrush objectListBgEndBrush; /*!< \brief Background brush for object list items in the end state (combined view only). */
+    QBrush objectListFgBothBrush; /*!< \brief Foreground brush for object list items in both states (combined view only). */
+    QBrush objectListBgBothBrush; /*!< \brief Background brush for object list items in both states (combined view only). */
 
-    QPen graphicsViewStartPen;
-    QBrush graphicsViewStartBrush;
-    QPen graphicsViewEndPen;
-    QBrush graphicsViewEndBrush;
-    QPen graphicsViewBothPen;
-    QBrush graphicsViewBothBrush;
+    QPen graphicsViewStartPen; /*!< \brief Pen for graphics view items in the start state (combined view only). */
+    QBrush graphicsViewStartBrush; /*!< \brief Brush for graphics view items in the start state (combined view only). */
+    QPen graphicsViewEndPen; /*!< \brief Pen for graphics view items in the end state (combined view only). */
+    QBrush graphicsViewEndBrush; /*!< \brief Brush for graphics view items in the end state (combined view only). */
+    QPen graphicsViewBothPen; /*!< \brief Pen for graphics view items in both states (combined view only). */
+    QBrush graphicsViewBothBrush; /*!< \brief Brush for graphics view items in both states (combined view only). */
 
 
 };
--- src/preferencesdialog.h
+++ src/preferencesdialog.h
@@ -28,48 +28,189 @@ namespace Ui {
 class PreferencesDialog;
 }
 
+
+/*!
+ * \brief Dialog class allowing the user to change the application settings.
+ *
+ * This class provides a user interface for the global Preferences object.
+ * The dialog reads from and writes to the Preferences object only.
+ * The Preferences object finally stores the values to disk.
+ * The application also gets the needed values from Preferences only and
+ * never from PreferencesDialog.
+ */
 class PreferencesDialog : public QDialog
 {
     Q_OBJECT
 
 public:
+    /*!
+     * \brief Creates a new PreferencesDialog.
+     * \param parent the parent widget
+     */
     explicit PreferencesDialog(QWidget* parent = nullptr);
+
+    /*!
+     * \brief Destructor.
+     */
     ~PreferencesDialog();
 
+    /*!
+     * \brief Loads the settings from the global Preferences instance.
+     */
     void loadSettings();
+
+    /*!
+     * \brief Write the settings to the global Preferences instance.
+     *
+     * This method is not invoked automatically when the user clicks the OK button.
+     * The method that opened the dialog has to test if the return value was QDialog::Accepted and call this method by itself.
+     *
+     * Does not write the settings to disk. Call Preferences::writeSettings() afterwards to do this.
+     */
     void saveSettings();
 
 private slots:
+    /*!
+     * \brief Target slot for selections in the object type list.
+     *
+     * This method retrieves the pen and brush for the selected object type with getSelectedPen() and getSelectedBrush()
+     * and updates the line width spin box, the style combo box and the color frames.
+     * It also enables or disables the Set Fill Color button depending on the selected type.
+     */
     void handleObjectListSelection();
+
+    /*!
+     * \brief Target slot for clicks on the Set Line Color button.
+     *
+     * Calls handleSetLayoutColor() with argument true.
+     */
     void handlePushButtonSetLineColorClicked();
+
+    /*!
+     * \brief Target slot for clicks on the Set Fill Color button.
+     *
+     * Calls handleSetLayoutColor() with argument false.
+     */
     void handlePushButtonSetFillColorClicked();
+
+    /*!
+     * \brief Target slot for clicks on the Set Foreground Color button.
+     *
+     * Calls handleSetObjectListColor() with argument true.
+     */
     void handlePushButtonSetForegroundColorClicked();
+
+    /*!
+     * \brief Target slot for clicks on the Set Background Color button.
+     *
+     * Calls handleSetObjectListColor() with argument false.
+     */
     void handlePushButtonSetBackgroundColorClicked();
+
+    /*!
+     * \brief Target slot for changes on the pen width and pen style.
+     *
+     * Writes the new properties with setSelectedPen().
+     */
     void penPropertiesChanged();
+
+    /*!
+     * \brief Target slot for clicks on the Add SMT button.
+     *
+     * Adds a new SMT solver with id, command and arguments to the list.
+     * Has no effect if the list already contains a solver with the same id.
+     */
     void addSmt();
+
+    /*!
+     * \brief Target slot for clicks on the Remove SMT button.
+     *
+     * Removes the selected SMT solver from the list.
+     */
     void removeSmt();
+
+    /*!
+     * \brief Target slot for clicks on the Edit SMT button.
+     *
+     * Updates the id, command and arguments of the selected SMT solver.
+     */
     void editSmt();
+
+    /*!
+     * \brief Target slot for selections in the SMT list.
+     *
+     * Updates the line edit widgets with the id, command and arguments from the selected SMT solver.
+     */
     void handleSmtListSelection();
 
 private:
+    /*!
+     * \brief Handle color changes for the track layout view.
+     *
+     * This method retrieves the pen and brush for the selected object type with getSelectedPen() and getSelectedBrush(),
+     * shows a color dialog to the user and writes back the new colors with setSelectedPen() and setSelectedBrush().
+     *
+     * \param linecolor true if the line color should be changed, false if the fill color should be changed
+     */
     void handleSetLayoutColor(bool linecolor);
+
+    /*!
+     * \brief Handle color changes for the object list view.
+     *
+     * This method retrieves the colors of the selected object directly from the list, shows a color dialog to the user
+     * and updated the list items with the new colors.
+     *
+     * \param foreground true if the foreground color should be changed, false if the background color should be changed
+     */
     void handleSetObjectListColor(bool foreground);
+
+    /*!
+     * \brief Get the list of languages which are currently installed.
+     *
+     * This method searches for the "ppview_*.qm" files in the "translations" directory.
+     *
+     * \return the list of languages.
+     */
     QStringList getAvailableLanguages();
+
+    /*!
+     * \brief Get the pen for the object type that is currently selected in the object type list.
+     *
+     * \return the pen for the selected object
+     */
     QPen getSelectedPen();
+
+    /*!
+     * \brief Set the pen for the object type that is currently selected in the object type list.
+     *
+     * \param p the pen for the selected object
+     */
     void setSelectedPen(QPen p);
+
+    /*!
+     * \brief Get the brush for the object type that is currently selected in the object type list.
+     *
+     * \return the brush for the selected object
+     */
     QBrush getSelectedBrush();
+
+    /*!
+     * \brief Set the brush for the object type that is currently selected in the object type list.
+     *
+     * \param b the brush for the selected object
+     */
     void setSelectedBrush(QBrush b);
 
-    Ui::PreferencesDialog* ui;
+    Ui::PreferencesDialog* ui; /*!< \brief The Qt Designer dialog. */
 
-    QHash<QString,QPen> changedPens;
-    QHash<QString,QBrush> changedBrushes;
+    QHash<QString,QPen> changedPens; /*!< \brief Hash map containing the currently changed pens. */
+    QHash<QString,QBrush> changedBrushes; /*!< \brief Hash map containing the currently changed brushes. */
 
-    QHash<QString,QString> changedSmtCommands;
-    QHash<QString,QString> changedSmtArguments;
+    QHash<QString,QString> changedSmtCommands; /*!< \brief Hash map containing the currently changed SMT commands. */
+    QHash<QString,QString> changedSmtArguments; /*!< \brief Hash map containing the currently changed SMT arguments. */
 
-    QStringList punktObjektNames;
-    QStringList bereichObjektNames;
+    QStringList punktObjektNames; /*!< \brief Constant list of all Punkt_Objekt subtype names. */
+    QStringList bereichObjektNames; /*!< \brief Constant list of all Bereich_Objekt subtype names. */
 };
 
 #endif // PREFERENCESDIALOG_H
